76e1bf3d48a963151038ae8bd5071610f0b156d8
prianshpate18 <priyansh.180804@gmail.com>
2026-01-13 15:27:37 +0530

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { siteConfig } from "@/config/siteConfig";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
+        <Analytics />
         <Providers>
           {children}
           <Toaster />
